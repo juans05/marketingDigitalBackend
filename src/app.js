c80f36c2851552a33aa36b37f455f28aa0ff56fc
juan saavedra <juans0520@gmail.com>
2026-03-22 23:29:27 -0500

    mejora

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use((req, res, next) => {
   const origin = req.headers.origin || '*';
   res.setHeader('Access-Control-Allow-Origin', origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Vary', 'Origin');
